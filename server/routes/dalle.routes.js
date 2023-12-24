60d5600aeb2db56fb457f08ec3a0c419dcbcb07e
Aithur <94808416+Manvinder-Partap@users.noreply.github.com>
2023-12-24 20:16:24 +1100

    fixed server

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi} from 'openai';
+import OpenAIApi from 'openai';
+
 
 dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
+
+
+const openai = new OpenAIApi({
   apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(config);
-
+})
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" })
 })
